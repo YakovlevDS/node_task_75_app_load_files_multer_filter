c363f85b946cb524097fe7ddd977ca5e97ebab88
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-21 09:37:41 +0300

    feat add multer filter load files

--- app.js
+++ app.js
@@ -1,12 +1,93 @@
-let nodePath = process.argv[0];
-let appPath = process.argv[1];
-let name = process.argv[2];
-let age = process.argv[3];
+// Здесь важно отметить, что, чтобы на стороне сервера multer
+//  мог подхватить загруженный файл, у формы атрибут enctype должен иметь значение multipart/form-data.
 
-console.log("nodePath: " + nodePath);
-console.log("appPath: " + appPath);
-console.log();
-console.log("name: " + name);
-console.log("age: " + age);
+// const express = require("express");
+// const multer  = require("multer");
+  
+// const app = express();
+ 
+// const upload = multer({dest:"uploads"});
+// app.use(express.static(__dirname));
+ 
+// app.post("/upload", upload.single("fileData"), (req, res, next)=> {
+   
+//     let fileData = req.file;
+ 
+//     console.log(fileData);
+//     if(!fileData)
+//         res.send("Ошибка при загрузке файла");
+//     else
+//         res.send("Файл загружен");
+// });
+// app.listen(3000);
+//! Настройка параметров сохранения файла diskStorage()
 
-// node app.js Tom 23
+// const express = require("express");
+// const multer  = require("multer");
+  
+// const app = express();
+ 
+// const storageConfig = multer.diskStorage({
+//     destination: (req, file, cb) =>{
+//         cb(null, "uploads");
+//     },
+//     filename: (req, file, cb) =>{
+//         cb(null, file.originalname);
+//     }
+// });
+ 
+// app.use(express.static(__dirname));
+ 
+// app.use(multer({storage:storageConfig}).single("fileData"));
+// app.post("/upload", function (req, res, next) {
+   
+//     let fileData = req.file;
+//     if(!fileData)
+//         res.send("Ошибка при загрузке файла");
+//     else
+//         res.send("Файл загружен");
+// });
+// app.listen(3000, ()=>{console.log("Server started");});
+
+// ! Фильтрация файлов
+const express = require("express");
+const multer  = require("multer");
+  
+const app = express();
+ 
+const storageConfig = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb(null, "uploads");
+    },
+    filename: (req, file, cb) =>{
+        cb(null, file.originalname);
+    }
+});
+// определение фильтра
+
+const fileFilter = (req, file, cb) => {
+  
+    if(file.mimetype === "image/png" || 
+    file.mimetype === "image/jpg"|| 
+    file.mimetype === "image/jpeg"){
+        cb(null, true);
+    }
+    else{
+        cb(null, false);
+    }
+ }
+ 
+app.use(express.static(__dirname));
+// И чтобы применить данный фильтр, его необходимо передать в объект multer параметру fileFilter:
+app.use(multer({storage:storageConfig, fileFilter: fileFilter}).single("fileData"));
+app.post("/upload", function (req, res, next) {
+   
+    let fileData = req.file;
+ 
+    console.log(fileData);
+    if(!fileData)
+        res.send("Ошибка при загрузке файла");
+    else
+        res.send("Файл загружен");
+});
+app.listen(3000, ()=>{console.log("Server started");});
